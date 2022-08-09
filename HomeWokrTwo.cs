cf6259e2256c9cff2073d9de0a646ca36caff6c6
Dmitry <woweuropeluffy@gmail.com>
2022-08-09 22:28:18 +0300

    Add solutions for HomeworkTwo

---
+++ HomeWokrTwo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_seminar
+{
+    public class HomeWokrTwo
+    {
+        /// <summary>
+        /// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+        /// 456 -> 5
+        /// 782 -> 8
+        /// 918 -> 1
+        /// </summary>
+        public void TaskTen()
+        {
+            Console.WriteLine("Задача номер 10");
+            var input = ReadUtils.ReadInt("Введите трёхзначное число!");
+            Console.WriteLine($"Второе число, это {input / 10 % 10}");
+        }
+
+        /// <summary>
+        /// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        /// 645 -> 5
+        /// 78 -> третьей цифры нет
+        /// 32679 -> 6
+        /// </summary>
+        public void TaskThirteen()
+        {
+            Console.WriteLine("Задача номер 13");
+            var input = ReadUtils.ReadInt("Введите число.");
+
+            if (input < 100)
+            {
+                Console.WriteLine("третей цифры нет.");
+                return;
+            }
+
+            while (input > 999)
+            {
+                input /= 10;
+            }
+
+            Console.WriteLine($"Третья цифра это: {input % 10}");
+        }
+
+        /// <summary>
+        /// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+        /// 6 -> да
+        /// 7 -> да
+        /// 1 -> нет
+        /// </summary>
+        public void TaskFifteen()
+        {
+            Console.WriteLine("Задача номер 15");
+            var input = ReadUtils.ReadInt("Введи цифру соответсвующую дню недели!");
+
+            switch (input)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine(":-( Рабочий день!");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine(":-) Выходной день!");
+                    break;
+                default:
+                    Console.WriteLine(":-/ Такого дня недели не существует!");
+                    break;
+            }
+        }
+    }
+}
---
+++ ReadUtils.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_seminar
+{
+    public class ReadUtils
+    {
+        public static int ReadInt(string introMessage, string errorMessage)
+        {
+            int result;
+            Console.WriteLine(introMessage);
+            while (!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine(errorMessage);
+                Console.WriteLine("Повторите попытку");
+            }
+            return result;
+        }
+
+        public static int ReadInt(string introMessage)
+        {
+            return ReadInt(introMessage, "Вы ввели не целое число или не числов вовсе.");
+        }
+
+        public static int[] ReadSequenseOfInt(string introMessage, string errorMessage)
+        {
+            Console.WriteLine(introMessage);
+            while (true)
+            {
+                var input = Console.ReadLine();
+                try
+                {
+                    var result = input.Split(' ', ',').Select(i => int.Parse(i.Trim())).ToArray();
+                    return result;
+                }
+                catch
+                {
+                    Console.WriteLine(errorMessage);
+                    Console.WriteLine("Повторите попытку!");
+                }
+            }
+
+        }
+
+        public static int[] ReadSequenseOfInt(string introMessage)
+        {
+            return ReadSequenseOfInt(introMessage, "Вы ввели ошибочные числа!");
+        }
+    }
+}
